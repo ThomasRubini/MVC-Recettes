87e8f9247e02b0d5578a255723c15fc0098c99e7
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-01-16 20:29:18 +0100

    move recipe data creation to RecipeModel and use real SQL database

--- Controllers/RecipeController.php
+++ Controllers/RecipeController.php
@@ -9,30 +9,13 @@ final class RecipeController
             echo "404";
             return;
         }
-        
+
         $O_recipeModel = new RecipeModel();
-        $O_recipe = $O_recipeModel->getByID($A_urlParams[0]);
-        if($O_recipe === null){
+        $A_returnArray = $O_recipeModel->getFullRecipeWithComments($A_urlParams[0]);
+        if ($A_returnArray === null) {
             echo "404";
             return;
         }
-
-        $O_ingredientModel = new IngredientModel();
-        $A_ingredients = $O_ingredientModel->searchByRecipe($O_recipe["id"]);
-
-        $O_userModel = new UserModel();
-        $A_authorName = $O_userModel->getNameByID($O_recipe["author_id"]);
-
-        $O_userModel = new DifficultyModel();
-        $A_difficultyName = $O_userModel->getByID($O_recipe["difficulty_id"]);
-
-        $A_returnArray = array(
-            "recipe_name" => $O_recipe["name"],
-            "recipe_desc" => $O_recipe["desc"],
-            "author_name" => $A_authorName,
-            "recipe_ingredients" => $A_ingredients,
-            "difficulty_name" => $A_difficultyName,
-        );
 
         View::show("recipe/view", $A_returnArray);
 
--- Views/recipe/view.php
+++ Views/recipe/view.php
@@ -1,10 +1,10 @@
-<p> <?= $A_view["recipe_name"] ?> </p>
-<p> Auteur: <?= $A_view["author_name"] ?> </p>
-<p> Difficulté: <?= $A_view["difficulty_name"] ?> </p>
+<p> <?= $A_view["NAME"] ?> </p>
+<p> Auteur: <?= $A_view["AUTHOR_NAME"] ?> </p>
+<p> Difficulté: <?= $A_view["DIFFICULTY_NAME"] ?> </p>
 <p> Ingrédients: </p>
 <?php
 
-foreach($A_view["recipe_ingredients"] as $i){
+foreach($A_view["INGREDIENTS"] as $i){
     echo "<p> {$i['name']}: {$i['quantity']} </p>";
 }
 ?>
