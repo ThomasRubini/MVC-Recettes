26096c30284cdd8802edde8c49f42ffc4a9558a5
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-01-18 11:33:28 +0100

    fix few features in the architecture, recipe is now in common

--- Views/common/recipe.php
+++ Views/common/recipe.php
@@ -1,5 +1,5 @@
-<a href="/recipe/view/1">
-    <img scr="<?= $A_view["img"] ?>">
+<a href="/recipe/view/<?= $A_view["id"]?>">
+    <img src="<?= $A_view["img"] ?>" alt="<?= $A_view["nom"]?>">
     <section>
         <h2> <?= $A_view["nom"]?> </h2>
         <p> <?= $A_view["note"]?> </p>
