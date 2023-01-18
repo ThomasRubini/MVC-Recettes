ab085e8591dc4e2096f1bc72f8c4c5fa8470be2e
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-01-18 11:21:07 +0100

    implement category with a data example

---
+++ Views/category/category.php
@@ -0,0 +1,47 @@
+<?php
+$array_recepies_Type_de_cuisson = array( //test
+    array(
+        "nom" => "Quaso",
+        "img" => "4.jpg",
+        "note" => "4.5"
+    ),
+    array(
+        "nom" => "Quaso",
+        "img" => "4.jpg",
+        "note" => "4.5"
+    ),
+    array(
+        "nom" => "Quaso",
+        "img" => "4.jpg",
+        "note" => "4.5"
+    )
+);
+
+$array_categories = array(
+    "Type de cuisson" => $array_recepies_Type_de_cuisson,
+    "Temps de préparation" => $array_recepies_Type_de_cuisson,
+    "Difficulté" => $array_recepies_Type_de_cuisson,
+    "Végan" => $array_recepies_Type_de_cuisson,
+    "Sans gluten" => $array_recepies_Type_de_cuisson,
+    "Sans lactose" => $array_recepies_Type_de_cuisson
+);
+?>
+
+<nav>
+    <ul>
+        <?php
+        foreach ($array_categories as $categoryName => $recipes) {
+            echo '<li><section>
+                <h1>$key</h1>
+                <ul>';
+                    foreach ($recipes as $recipe) {
+                        echo '<li>';
+                            View::show("../common/recipe", $recipe);
+                        echo '</li>';
+                    }
+                echo '</ul>
+            </section></li>';
+        }
+        ?>
+    </ul>
+</nav>
