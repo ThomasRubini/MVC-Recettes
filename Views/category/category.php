ca1c97b94dd77e6941082e6edc3eea1357e78b4c
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-01-19 16:07:54 +0100

    category page

--- Views/category/category.php
+++ Views/category/category.php
@@ -1,16 +1,19 @@
 <?php
 $array_recepies_Type_de_cuisson = array( //test
     array(
+        "id" => "1",
         "nom" => "Quaso",
         "img" => "4.jpg",
         "note" => "4.5"
     ),
     array(
+        "id" => "1",
         "nom" => "Quaso",
         "img" => "4.jpg",
         "note" => "4.5"
     ),
     array(
+        "id" => "1",
         "nom" => "Quaso",
         "img" => "4.jpg",
         "note" => "4.5"
@@ -32,11 +35,11 @@ $array_categories = array(
         <?php
         foreach ($array_categories as $categoryName => $recipes) {
             echo '<li><section>
-                <h1>$key</h1>
+                <h1><?$key?></h1>
                 <ul>';
                     foreach ($recipes as $recipe) {
                         echo '<li>';
-                            View::show("../common/recipe", $recipe);
+                            View::show("common/recipe", $recipe);
                         echo '</li>';
                     }
                 echo '</ul>
