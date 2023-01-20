637782f9071657add9f8163410a2d1e9307d5682
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-01-20 18:42:20 +0100

    refactor admin checking function to Session

--- Controllers/ManageUserController.php
+++ Controllers/ManageUserController.php
@@ -3,16 +3,6 @@
 final class ManageUserController
 {
 
-    public static function admin_or_die(){
-        Session::login_or_die();
-
-        $O_userModel = new UserModel();
-        if (!$O_userModel->isUserAdmin($_SESSION["ID"])) {
-            header("Location: /");
-            die();
-        }
-    }
-
     private function getOrDie($DICT, $key)
     {
         if (isset($DICT[$key])) return $DICT[$key];
@@ -21,14 +11,15 @@ final class ManageUserController
 
     public function defaultAction(Array $A_urlParams = null, Array $A_postParams = null)
     {
-        self::admin_or_die();
+        Session::admin_or_die();
 
         return View::show("manageUser/default");
     }
 
     public function searchAction(Array $A_urlParams = null, Array $A_postParams = null)
     {
-        self::admin_or_die();
+        Session::admin_or_die();
+        
         $S_search = self::getOrDie($_POST, "search");
         echo "Terme de recherche choisi: $S_search";
     }
