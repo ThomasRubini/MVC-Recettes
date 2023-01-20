8a5d6e4cfcb069b2e8a78cc6dd59ea814164fb13
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-01-20 19:11:03 +0100

    Alias default action to search action in ManagerUser controller

--- Controllers/ManageUserController.php
+++ Controllers/ManageUserController.php
@@ -3,11 +3,9 @@
 final class ManageUserController
 {
 
-    public function defaultAction(Array $A_urlParams = null, Array $A_postParams = null)
+    public function defaultAction(Array $A_urlParams = null, Array $A_postParams = null, Array $A_getParams = null)
     {
-        Session::admin_or_die();
-
-        return View::show("manageUser/search");
+        self::searchAction($A_urlParams, $A_postParams, $A_getParams);
     }
 
     public function searchAction(Array $A_urlParams = null, Array $A_postParams = null, Array $A_getParams = null)
@@ -22,19 +20,21 @@ final class ManageUserController
     
     private function searchViewAction(Array $A_urlParams = null, Array $A_postParams = null, Array $A_getParams = null)
     {
-
+        View::show("manageUser/search");
     }
     
     private function searchQueryViewAction(Array $A_urlParams = null, Array $A_postParams = null, Array $A_getParams = null)
     {
         $S_query = $A_getParams["query"];
-
+        
         $O_userModel = new UserModel();
         $A_results = $O_userModel->searchUsers($S_query);
-
+        
         var_dump($A_results);
-
+        
         echo "Terme de recherche choisi: $S_query";
+
+        View::show("manageUser/search");
     }
 
 }
