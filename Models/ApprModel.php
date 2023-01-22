e6f735c3aff330f79f43668279026d1acc6955e3
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-01-22 20:57:22 +0100

    update ApprModel

--- Models/ApprModel.php
+++ Models/ApprModel.php
@@ -1,16 +1,21 @@
 <?php
 
-final class AppreciationsModel {
-    public function getRecipeAppreciations($I_recipe_id){
+final class ApprModel {
+    public function getRecipeApprs($I_recipe_id)
+    {
         $O_model = Model::get();
         $stmt = $O_model->prepare("SELECT * FROM APPPRECIATION WHERE ID = :recipe_id");
         $stmt->bindParam("recipe_id",$I_recipe_id);
         $stmt->execute();
         return $stmt->fetch();
     }
-    public function createAppreciation($I_recipe_id,$I_user_id,$S_Comment,$I_score){
+
+    public function createAppr($I_recipe_id, $I_user_id, $S_Comment, $I_score)
+    {
         $O_model = Model::get();
-        $stmt = $O_model->prepare("INSERT INTO APPPRECIATION(COMMENT,SCORE,DATE,AUTHOR_ID,RECIPE_ID) VALUES(:comment,:score,:date,:author_id,:recipe_id)");
+        $stmt = $O_model->prepare("
+            INSERT INTO APPPRECIATION (COMMENT,SCORE,DATE,AUTHOR_ID,RECIPE_ID) VALUES (:comment, :score, :date, :author_id, :recipe_id)
+        ");
         $stmt->bindParam("comment",$S_Comment);
         $stmt->bindParam("score",$I_score);
         $_date = date("Y-m-d");
@@ -20,13 +25,17 @@ final class AppreciationsModel {
         $stmt->execute();
         return $stmt->fetch();
     }
-    public function deleteAppreciation($I_recipe_id){
+
+    public function deleteAppr($I_appr_id)
+    {
         $O_model = Model::get();
-        $stmt = $O_model->prepare("DELETE FROM APPPRECIATION WHERE ID = :recipe_id");
-        $stmt->bindParam("recipe_id",$I_recipe_id);
+        $stmt = $O_model->prepare("DELETE FROM APPPRECIATION WHERE ID = :appr_id");
+        $stmt->bindParam("appr_id", $I_appr_id);
         $stmt->execute();
     }
-    public function updateAppreciation($I_recipe_id){
+
+    public function updateAppreciation($I_appr_id)
+    {
 
     }
 }
