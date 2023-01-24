28a7e5d0841a22d98bdc091138c41f0dddd07739
AudricV <74829229+AudricV@users.noreply.github.com>
2023-01-24 18:33:30 +0100

    Add manage users view
    Also remove the search view, merged into this new view.

---
+++ Views/manageUser/manage_users.php
@@ -0,0 +1,46 @@
+<?php
+//TODO: sample data, replace by real data and real keys
+$username_requested = "A username";
+$user_accounts_results = array(
+    array("NAME" => "A name",
+        "AVATAR" => '/static/img/users/1.jpg',
+        "USER_ID" => 4234273));
+?>
+<main>
+    <h1 class="accounts_management_title">Gestion de comptes</h1>
+    <form method="GET" action="manage_users">
+        <label for="username">Saisissez un nom d'utilisateur</label>
+        <input type="search" id="username" name="query" placeholder="Nom de l'utilisateur">
+    </form>
+    <section>
+        <?php
+        if ($username_requested === null) {
+            echo '<h2 class="username_required_title">Nom d\'utilisateur requis</h2>
+        <p class="username_required_description">Un nom d\'utilisateur est requis pour gérer des utilisateurs.</p>';
+        } else {
+            if (empty($user_accounts_results)) {
+                echo '<h2 class="no_user_results_title">Aucun résultat</h2>
+            <p class="no_user_results_description">Vérifiez l\'orthographe et la casse de votre saisie.</p>';
+            } else {
+                echo '<ul class="user_account_list">';
+
+                foreach ($user_accounts_results as $key => $user_account_result) {
+                    $user_account_name = $user_account_result["NAME"];
+                    echo '<li class="user_account" data-id="' . $user_account_result["USER_ID"] . '">
+                <img class="user_acccount_picture" src=' . $user_account_result["AVATAR"] . ' alt="Photo de profil de ' . $user_account_name . '">
+                <h3 class="user_account_name">' . $user_account_name . '</h3>
+                </li>';
+                }
+
+                echo '</ul>
+            <form method="POST" action="manage_users">
+                <input id="accounts_to_manage" type="text" hidden>
+                <input type="submit" name="enable" value="Activer">
+                <input type="submit" name="disable" value="Désactiver">
+                <input type="submit" value="Supprimer">
+            </form>';
+            }
+        }
+        ?>
+    </section>
+</main>
--- Views/manageUser/search.php
+++
@@ -1,7 +0,0 @@
-<p> Default view </p>
-
-<p> Please search : </p>
-<form action="/manageUser/search" method="GET">
-    <input name="query">
-    <input type="submit" value="Chercher">
-</form>
