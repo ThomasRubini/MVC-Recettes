4c3590b3a5cdd26381f8071b813fcb1bd96ced43
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-01-24 19:41:12 +0100

    update view to user updated model

--- Views/manageUser/manage_users.php
+++ Views/manageUser/manage_users.php
@@ -1,29 +1,28 @@
 <main>
     <h1 class="accounts_management_title">Gestion de comptes</h1>
-    <form method="GET" action="manage_users">
+    <form method="GET" action="/manageUser/search">
         <label for="username">Saisissez un nom d'utilisateur</label>
         <input type="search" id="username" name="query" placeholder="Nom de l'utilisateur">
     </form>
     <section>
         <?php
-        $username_requested = $A_view["QUERY"];
-        if ($username_requested === null) {
+        $S_query = $A_view["QUERY"];
+        if ($S_query === null) {
             echo '<h2 class="username_required_title">Nom d\'utilisateur requis</h2>
         <p class="username_required_description">Un nom d\'utilisateur est requis pour gérer des utilisateurs.</p>';
         } else {
-            $user_accounts_results = $A_view["RESULTS"];
-            echo '<h3 class="username_searched">' . $username_requested . '</h3>';
-            if (empty($user_accounts_results)) {
+            $A_results = $A_view["RESULTS"];
+            echo '<h3 class="username_searched">' . $S_query . '</h3>';
+            if (empty($A_results)) {
                 echo '<h2 class="no_user_results_title">Aucun résultat</h2>
             <p class="no_user_results_description">Vérifiez l\'orthographe et la casse de votre saisie.</p>';
             } else {
                 echo '<ul class="user_account_list">';
 
-                foreach ($user_accounts_results as $key => $user_account_result) {
-                    $user_account_name = $user_account_result["NAME"];
-                    echo '<li class="user_account" data-id="' . $user_account_result["USER_ID"] . '">
-                <img class="user_acccount_picture" src=' . $user_account_result["AVATAR"] . ' alt="Photo de profil de ' . $user_account_name . '">
-                <h3 class="user_account_name">' . $user_account_name . '</h3>
+                foreach ($A_results as $O_user) {
+                    echo '<li class="user_account" data-id="' . $O_user->I_ID . '">
+                <img class="user_acccount_picture" src=' . $O_user->getProfilePicLink() . ' alt="Photo de profil de ' . $O_user->S_USERNAME . '">
+                <h3 class="user_account_name">' . $O_user->S_USERNAME . '</h3>
                 </li>';
                 }
 
