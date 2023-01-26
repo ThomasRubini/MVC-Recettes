5b9e412ca2359afd2c4ddb3191005e2ddb4b5ecb
AudricV <74829229+AudricV@users.noreply.github.com>
2023-01-26 15:39:12 +0100

    Improve error pages
    - Wrap messages into a main element
    - Add classes for all texts except the back to home link
    - Translate everything in French

--- Views/errors/400.php
+++ Views/errors/400.php
@@ -1,8 +1,10 @@
-<h1>Error 400</h1>
-<h2>Erreur du client 😥</h2>
-<?php
+<main class="error_main">
+    <h1 class="error_code_title">Erreur 400</h1>
+    <h2 class="error_code_description">Erreur du client 😥</h2>
+    <?php
     if (isset($A_view)) {
-        echo "<p> message d'erreur: $A_view </p>";
+        echo '<p class="error_message">Message d\'erreur&nbsp;: ' . $A_view . '</p>';
     }
-?>
-<a href="/">Retourner à l'accueil<a>
+    ?>
+    <a href="/">Retourner à l'accueil</a>
+</main>
--- Views/errors/404.php
+++ Views/errors/404.php
@@ -1,5 +1,5 @@
-<main>
-    <h1>Error 404</h1>
-    <h2>La page n'a pas été trouvée 😥</h2>
+<main class="error_main">
+    <h1 class="error_code_title">Erreur 404</h1>
+    <h2 class="error_code_description">La page n'a pas été trouvée 😥</h2>
     <a href="/">Retourner à l'accueil</a>
 </main>
