8b2940b251283501e0ea50eb23805956f18ee1bd
Capelier-Marla <marla.capelier@etu.univ-amu.fr>
2023-01-26 16:54:55 +0100

    replace names for errors main

--- Views/errors/403.php
+++ Views/errors/403.php
@@ -1,5 +1,5 @@
-<main class="error_main">
-    <h1 class="error_code_title">Erreur 403</h1>
-    <h2 class="error_code_description">Vous n'avez pas l'autorisation d'accéder à cette page 😥</h2>
+<main class="errorPage">
+    <h1>Erreur 403</h1>
+    <h2>Vous n'avez pas l'autorisation d'accéder à cette page 😥</h2>
     <a href="/">Retourner à l'accueil</a>
 </main>
--- Views/errors/500.php
+++ Views/errors/500.php
@@ -1,9 +1,9 @@
-<main class="error_main">
-    <h1 class="error_code_title">Erreur 500</h1>
-    <h2 class="error_code_description">Erreur interne du serveur 😥</h2>
+<main class="errorPage">
+    <h1>Erreur 500</h1>
+    <h2>Erreur interne du serveur 😥</h2>
     <?php
     if (isset($A_view)) {
-        echo '<p class="error_message">Message d\'erreur&nbsp;: ' . $A_view .'</p>';
+        echo '<p>Message d\'erreur&nbsp;: ' . $A_view .'</p>';
     }
     ?>
     <a href="/">Retourner à l'accueil<a>
